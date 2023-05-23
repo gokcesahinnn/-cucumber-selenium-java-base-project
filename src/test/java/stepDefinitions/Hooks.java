6052fddcfea1f0705f837759ac6337fb52b7c5e1
gokcesahinnn <111432634+gokcesahinnn@users.noreply.github.com>
2023-05-23 10:08:09 +0300

    screen shot eklendı

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -8,6 +8,11 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utilities.Driver;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 import static utilities.Driver.getDriver;
@@ -25,14 +30,45 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(Scenario scenario) {
-
-        final byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+    public void takeScreenshot(Scenario scenario) {
         if (scenario.isFailed()) {
-            scenario.attach(screenshot, "image/png", "screenshots");
+            // Ekran görüntüsünü al
+            Object driver = webDriver;
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+
+            // Ekran görüntüsünün dosya adını oluştur
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+            LocalDateTime now = LocalDateTime.now();
+            String timestamp = dtf.format(now);
+            String screenshotName = "screenshot_" + timestamp + ".png";
+
+            // Ekran görüntüsünü klasöre kaydet
+            String screenshotFolderPath = "screenshots/";
+            createFolder(screenshotFolderPath);
+            String screenshotPath = screenshotFolderPath + screenshotName;
+            saveScreenshot(screenshot, screenshotPath);
+
+            // Senaryonun ekran görüntüsünü rapora ekle
+            scenario.attach(screenshot, "image/png", screenshotName);
+        }
+    }
+
+    private void createFolder(String folderPath) {
+        try {
+            Path path = Paths.get(folderPath);
+            Files.createDirectories(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveScreenshot(byte[] screenshot, String screenshotPath) {
+        try {
+            Files.write(Paths.get(screenshotPath), screenshot);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         Driver.closeDriver();
-
-
     }
+
 }
